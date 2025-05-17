b817d3b8639340b0d347b89acc0d29e1a0809d42
dhop83 <dale1383@gmail.com>
2025-05-17 10:44:15 -0500

    Delete frontend directory

--- frontend/index.js
+++
@@ -1,6 +0,0 @@
-fetch("https://entitlement-backend.onrender.com/")
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById("root").innerHTML = `<h1>${data.message}</h1>`;
-    })
-    .catch(error => console.error("Error:", error));
